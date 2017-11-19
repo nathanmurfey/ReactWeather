9a4b9fead165f479074e51ed83635a888ce8e10f
Nathan Murfey <murfey.nathan@gmail.com>
2017-11-19 13:59:32 +1100

    rows and columns for the examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples!</h3>
-            <p>Welcome to examples page</p>
+            <h1 className="text-center">Examples!</h1>
+            <p>here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Melbourne">Melbourne, AUS</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Sydney">Sydeny, AUS</Link>
+                </li>
+            </ol>
         </div>
     );
 }
