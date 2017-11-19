4582f5da3d85371f284b0ac475dee4a4dc6214a8
Nathan Murfey <murfey.nathan@gmail.com>
2017-11-19 14:25:08 +1100

    changing the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = props => {
     return(
         <div>
-            <h3>About Component</h3>
-            <p>This is the about page</p>
+            <h1 className="text-center">About!</h1>
+            <p>This is a weather application built on react, I built this as part of the complete react web developer course</p>
+            <p>Here are some of the tools I used</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used.
+                </li>
+                <li>
+                    <a href="https://openweathermap.org/">OpenWeatherMap</a> - This was the Weather API that I used.
+                </li>
+            </ul>
         </div>
     );
 }
